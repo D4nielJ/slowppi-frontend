01091bd2e11cfc169492f70fa777598e303033cd
d4nielj <d4niel.djm@gmail.com>
2021-12-06 10:44:40 -0500

    Add chakra provider and theme extension

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import store from './utils/store/store';
+import theme from './lib/theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
       </Router>
     </Provider>
   </React.StrictMode>,
---
+++ src/lib/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Righteous',
+    body: 'Open Sans',
+  },
+});
+
+export default theme;
