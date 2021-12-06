597c18422a32574465ecd2951ec85334ff141685
d4nielj <d4niel.djm@gmail.com>
2021-12-06 11:29:45 -0500

    Style buttons

--- src/components/Home/Button.js
+++ src/components/Home/Button.js
@@ -1,7 +1,7 @@
 import { Button as ChakraButton } from '@chakra-ui/react';
 
 const Button = ({ children }) => (
-  <ChakraButton color="white" bg="green.400" rounded="full" fontSize="xl">
+  <ChakraButton color="white" bg="green.400" rounded="full" fontSize="xl" py={6}>
     {children}
   </ChakraButton>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import {
-  VStack, HStack, Heading,
+  VStack, HStack, Heading, Icon, Text,
 } from '@chakra-ui/react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { IoEnterOutline, IoChevronForwardCircleOutline } from 'react-icons/io5';
+import { AiOutlineUserAdd } from 'react-icons/ai';
 import { logoutUser } from '../utils/actions/currentUser.actions';
 import Button from '../components/Home/Button';
 
@@ -21,13 +23,25 @@ const Home = () => {
       justifyContent="center"
       spacing={12}
     >
-      <Heading fontSize="6xl" letterSpacing="2rem" color="white">SLOWPPI</Heading>
+      <Heading fontSize="7xl" letterSpacing="2.5rem" color="white">SLOWPPI</Heading>
       <HStack spacing={8}>
         <Link to="/login">
-          <Button>Log in</Button>
+          <Button>
+            <HStack spacing={4}>
+              <Icon as={IoEnterOutline} fontSize="2xl" />
+              <Text>Log in</Text>
+              <Icon as={IoChevronForwardCircleOutline} fontSize="2xl" />
+            </HStack>
+          </Button>
         </Link>
         <Link to="/signup">
-          <Button>Sign up</Button>
+          <Button>
+            <HStack spacing={4}>
+              <Icon as={AiOutlineUserAdd} fontSize="2xl" />
+              <Text>Sign up</Text>
+              <Icon as={IoChevronForwardCircleOutline} fontSize="2xl" />
+            </HStack>
+          </Button>
         </Link>
       </HStack>
 
