ea42e166cf865facef7a1209423b5d5446be6fc1
d4nielj <d4niel.djm@gmail.com>
2021-12-06 11:40:16 -0500

    Add green colors to theme

--- src/components/Home/Button.js
+++ src/components/Home/Button.js
@@ -1,7 +1,7 @@
 import { Button as ChakraButton } from '@chakra-ui/react';
 
 const Button = ({ children }) => (
-  <ChakraButton color="white" bg="green.400" rounded="full" fontSize="xl" py={6}>
+  <ChakraButton color="white" bg="green.300" rounded="full" fontSize="xl" py={6} _hover={{ bg: 'green.400' }} _active={{ bg: 'green.500' }}>
     {children}
   </ChakraButton>
 );
--- src/lib/theme.js
+++ src/lib/theme.js
@@ -5,6 +5,15 @@ const theme = extendTheme({
     heading: 'Righteous',
     body: 'Open Sans',
   },
+  colors: {
+    green: {
+      100: '#D2F368',
+      200: '#C2EE2F',
+      300: '#98C010',
+      400: '#77970C',
+      500: '#4A5E08',
+    },
+  },
 });
 
 export default theme;
