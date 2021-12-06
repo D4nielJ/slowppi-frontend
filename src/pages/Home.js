508aa880b3e8c3c2a456d048be707355c9208f80
d4nielj <d4niel.djm@gmail.com>
2021-12-05 20:00:53 -0500

    Show log status in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logoutUser } from '../utils/actions/currentUser.actions';
 
 const Home = () => {
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.currentUser);
 
   const handleLogout = () => {
     dispatch(logoutUser());
@@ -11,6 +12,9 @@ const Home = () => {
 
   return (
     <section>
+      {user && (
+        <div>Logged in</div>
+      )}
       <Link to="/login">Login</Link>
       <Link to="/signup">Sign up</Link>
       <button type="button" onClick={handleLogout}>Log out</button>
