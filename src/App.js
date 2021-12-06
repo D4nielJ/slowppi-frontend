02a18c6a47d7fcf494177f7cfe4f065de9a66ca2
d4nielj <d4niel.djm@gmail.com>
2021-12-06 17:19:56 -0500

    Clean code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Box, VStack } from '@chakra-ui/react';
+import { VStack } from '@chakra-ui/react';
 import { Home, Login, Signup } from './pages';
 import '@fontsource/righteous';
 import '@fontsource/open-sans/300.css';
--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -1,7 +1,4 @@
 import RegistrationNavbar from './RegistrationNavbar';
 import Button from './Button';
 
-// Either change imports if no more exports in the future
-// or remove disabled once more exports
-/* eslint-disable */
 export { RegistrationNavbar, Button };
