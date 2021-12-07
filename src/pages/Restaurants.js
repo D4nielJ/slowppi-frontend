cc738f7e0f33bea02686fe38fd87e7e22661f704
d4nielj <d4niel.djm@gmail.com>
2021-12-07 15:23:33 -0500

    Remove Auth from restaurants

--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -1,16 +1,9 @@
-import { useSelector } from 'react-redux';
 import Layout from '../components/Layout/Layout';
-import { useAuth } from '../utils/customHooks';
 
-const Restaurants = () => {
-  useAuth('/', ['', 'admin']);
-
-  const { user } = useSelector((state) => state.currentUser);
-  return (
-    <Layout>
-      Hello world
-    </Layout>
-  );
-};
+const Restaurants = () => (
+  <Layout>
+    Hello world
+  </Layout>
+);
 
 export default Restaurants;
