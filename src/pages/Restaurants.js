66f01c861a8a8249b90bc02ba74571fc6cf6ae8a
leonardo <leonardodias@ucl.br>
2021-12-08 16:46:02 -0300

    Create useEffect hoot to dispatch shift actions when load restaurants page

--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchShifts } from '../utils/actions/shifts.actions';
 import Layout from '../components/Layout/Layout';
 import { useAuth } from '../utils/customHooks';
 
 const Restaurants = () => {
   useAuth('/', ['', 'admin']);
+  const { shifts } = useSelector((state) => state.shifts);
+
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.currentUser);
+
+  useEffect(() => {
+    if (shifts.length === 0) {
+      dispatch(fetchShifts(user));
+    }
+  }, [shifts, user]);
 
   return (
     <Layout>
