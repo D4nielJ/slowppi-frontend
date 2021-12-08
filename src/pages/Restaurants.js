e63e2bfc0b395a10ddcdf6b09fd32d85a19d837a
leonardo <leonardodias@ucl.br>
2021-12-08 19:12:18 -0300

    Add dispatch restaurant reducer to component

--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchShifts } from '../utils/actions/shifts.actions';
 import { fetchCategories } from '../utils/actions/categories.actions';
+import { fetchRestaurants } from '../utils/actions/restaurants.actions';
 import Layout from '../components/Layout/Layout';
 import { useAuth } from '../utils/customHooks';
 
@@ -9,6 +10,7 @@ const Restaurants = () => {
   useAuth('/', ['', 'admin']);
   const { shifts } = useSelector((state) => state.shifts);
   const { categories } = useSelector((state) => state.categories);
+  const { restaurants } = useSelector((state) => state.restaurants);
 
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.currentUser);
@@ -20,7 +22,10 @@ const Restaurants = () => {
     if (categories.length === 0) {
       dispatch(fetchCategories(user));
     }
-  }, [shifts, categories, user]);
+    if (restaurants.length === 0) {
+      dispatch(fetchRestaurants(user));
+    }
+  }, [shifts, categories, restaurants, user]);
 
   return (
     <Layout>
