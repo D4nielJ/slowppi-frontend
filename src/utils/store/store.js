186392da610d97a49a5558eabf9658f864367f3c
leonardo <leonardodias@ucl.br>
2021-12-08 19:11:55 -0300

    Add restaurants reducers to store

--- src/utils/store/store.js
+++ src/utils/store/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { categoriesReducer, currentUserReducer, shiftsReducer } from '../reducers';
+import {
+  categoriesReducer,
+  currentUserReducer,
+  restaurantsReducer,
+  shiftsReducer,
+} from '../reducers';
 
 const store = configureStore({
   reducer: {
     currentUser: currentUserReducer,
     shifts: shiftsReducer,
     categories: categoriesReducer,
+    restaurants: restaurantsReducer,
   },
 });
 
