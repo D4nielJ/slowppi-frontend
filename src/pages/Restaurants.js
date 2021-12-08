f820ac4f53e2aaaacc9129acedc40d950e77f839
leonardo <leonardodias@ucl.br>
2021-12-08 17:25:13 -0300

    Dispatch fetch categories on restaurants component

--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchShifts } from '../utils/actions/shifts.actions';
+import { fetchCategories } from '../utils/actions/categories.actions';
 import Layout from '../components/Layout/Layout';
 import { useAuth } from '../utils/customHooks';
 
 const Restaurants = () => {
   useAuth('/', ['', 'admin']);
   const { shifts } = useSelector((state) => state.shifts);
+  const { categories } = useSelector((state) => state.categories);
 
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.currentUser);
@@ -15,7 +17,10 @@ const Restaurants = () => {
     if (shifts.length === 0) {
       dispatch(fetchShifts(user));
     }
-  }, [shifts, user]);
+    if (categories.length === 0) {
+      dispatch(fetchCategories(user));
+    }
+  }, [shifts, categories, user]);
 
   return (
     <Layout>
