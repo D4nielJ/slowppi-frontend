96d790ba16c894e07dbc9301b0336895c7ae8a5a
leonardo <leonardodias@ucl.br>
2021-12-08 16:43:55 -0300

    add shift constants to index to be exported

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -1,6 +1,7 @@
 import currentUserConstants from './currentUser.constants';
+import shiftsConstants from './shifts.constants';
 
 // Either change imports if no more exports in the future
 // or remove disabled once more exports
 /* eslint-disable */
-export { currentUserConstants };
+export { currentUserConstants, shiftsConstants };
