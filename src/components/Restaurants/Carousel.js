1b563eab169f528a2283cbdd5a34415ca091d59a
d4nielj <d4niel.djm@gmail.com>
2021-12-09 17:56:16 -0500

    Try to animate slider

--- src/components/Restaurants/Carousel.js
+++ src/components/Restaurants/Carousel.js
@@ -1,12 +1,20 @@
 import { HStack } from '@chakra-ui/react';
+import { AnimatePresence, motion } from 'framer-motion';
 import RestCard from './RestaurantCard';
 
+const MotionHStack = motion(HStack);
+
 const Carousel = ({ rests }) => (
-  <HStack spacing={28}>
-    {rests && rests.map((rest) => (
-      <RestCard key={rest.id} rest={rest} />
-    ))}
-  </HStack>
+
+  <MotionHStack
+    spacing={28}
+  >
+    <AnimatePresence initial={false}>
+      {rests && rests.map((rest) => (
+        <RestCard key={rest.id} rest={rest} />
+      ))}
+    </AnimatePresence>
+  </MotionHStack>
 );
 
 export default Carousel;
--- src/components/Restaurants/RestaurantCard.js
+++ src/components/Restaurants/RestaurantCard.js
@@ -1,26 +1,68 @@
-import { Text, HStack, VStack, Box, Image, AspectRatio, Icon } from "@chakra-ui/react";
-import { IoLogoFacebook, IoLogoInstagram, IoLogoTwitter } from "react-icons/io5";
+import {
+  Text, HStack, VStack, Box, Image, AspectRatio, Icon,
+} from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import { IoLogoFacebook, IoLogoInstagram, IoLogoTwitter } from 'react-icons/io5';
 
-const RestCard = ({ rest: { id, name, image, description } }) => (
-  <VStack spacing={4}>
+const MotionVStack = motion(VStack);
+
+const RestCard = ({
+  rest: {
+    id, name, image, description,
+  },
+}) => (
+  <MotionVStack
+    initial={{ x: 300, opacity: 0 }}
+    animate={{ x: 0, opacity: 1 }}
+    exit={{ x: -300, opacity: 0 }}
+    transition={{ type: 'spring', duration: 0.4 }}
+    spacing={4}
+  >
     <AspectRatio w={52} ratio={1}>
       <Image src={image} alt={description} w="full" h="full" objectFit="cover" />
     </AspectRatio>
     <Box textAlign="center">
-      <Text as="h3" mb={6} fontWeight="black" fontSize="lg" textTransform="uppercase" letterSpacing={1} >{name}</Text>
+      <Text as="h3" mb={6} fontWeight="black" fontSize="lg" textTransform="uppercase" letterSpacing={1}>{name}</Text>
       <Text color="gray.400" fontWeight="500">{description}</Text>
     </Box>
     <HStack spacing={5} color="gray.400">
       <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-        <Icon as={IoLogoFacebook} w={7} h={7} p={1} borderWidth={2} borderColor="gray.400" rounded="full" _hover={{ color: 'gray.500', borderColor: 'gray.500' }} />
+        <Icon
+          as={IoLogoFacebook}
+          w={7}
+          h={7}
+          p={1}
+          borderWidth={2}
+          borderColor="gray.400"
+          rounded="full"
+          _hover={{ color: 'gray.500', borderColor: 'gray.500' }}
+        />
       </a>
       <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-        <Icon as={IoLogoTwitter} w={7} h={7} p={1} borderWidth={2} borderColor="gray.400" rounded="full" _hover={{ color: 'gray.500', borderColor: 'gray.500' }} />
+        <Icon
+          as={IoLogoTwitter}
+          w={7}
+          h={7}
+          p={1}
+          borderWidth={2}
+          borderColor="gray.400"
+          rounded="full"
+          _hover={{ color: 'gray.500', borderColor: 'gray.500' }}
+        />
       </a>
       <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
-        <Icon as={IoLogoInstagram} w={7} h={7} p={1} borderWidth={2} borderColor="gray.400" rounded="full" _hover={{ color: 'gray.500', borderColor: 'gray.500' }} />
+        <Icon
+          as={IoLogoInstagram}
+          w={7}
+          h={7}
+          p={1}
+          borderWidth={2}
+          borderColor="gray.400"
+          rounded="full"
+          _hover={{ color: 'gray.500', borderColor: 'gray.500' }}
+        />
       </a>
     </HStack>
-  </VStack>
+  </MotionVStack>
 );
 export default RestCard;
