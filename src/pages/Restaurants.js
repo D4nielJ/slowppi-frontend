cf8ed53364717748f650ba0245a0ffe69e4658b9
d4nielj <d4niel.djm@gmail.com>
2021-12-09 10:48:10 -0500

    Clean code

--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -9,29 +9,11 @@ import { useAuth } from '../utils/customHooks';
 
 const Restaurants = () => {
   useAuth('/', ['', 'admin']);
+  const dispatch = useDispatch();
   const { shifts } = useSelector((state) => state.shifts);
   const { categories } = useSelector((state) => state.categories);
   const { restaurants } = useSelector((state) => state.restaurants);
-
-  const path = useLocation();
-  const currentPage = path.search.split('=')[1];
-  const previousPage = parseInt(currentPage, 10) - 1;
-  const nextPage = parseInt(currentPage, 10) + 1;
-
-  const dispatch = useDispatch();
   const { user } = useSelector((state) => state.currentUser);
-
-  const handlePageClick = async (data) => {
-    if (data.target.text === 'PREVIOUS') {
-      if (previousPage > 0) {
-        dispatch(fetchRestaurants(user, previousPage));
-      }
-    }
-
-    if (data.target.text === 'NEXT') {
-      dispatch(fetchRestaurants(user, nextPage));
-    }
-  };
 
   useEffect(() => {
     if (shifts.length === 0) {
@@ -47,24 +29,7 @@ const Restaurants = () => {
 
   return (
     <Layout>
-      <RouterLink to="?page=1" onClick={handlePageClick}>PREVIOUS</RouterLink>
-      {restaurants.map((item) => (
-        <div key={item.id}>
-          <div>
-            <p>
-              Name:
-              {item.name}
-            </p>
-          </div>
-          <div>
-            <p>
-              Description:
-              {item.description}
-            </p>
-          </div>
-        </div>
-      ))}
-      <RouterLink to="?page=2" onClick={handlePageClick}>NEXT</RouterLink>
+      
     </Layout>
   );
 };
