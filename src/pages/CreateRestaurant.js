73cda7cf7afed55ea5d88a66e0c60552bc6bd9f4
Alonso Falconi <alonso.fl@live.com>
2021-12-10 11:07:08 -0500

    Add selectors for shifts, categories and user

--- src/pages/CreateRestaurant.js
+++ src/pages/CreateRestaurant.js
@@ -1,8 +1,30 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchShifts } from '../utils/actions/shifts.actions';
+import { fetchCategories } from '../utils/actions/categories.actions';
 import Layout from '../components/Layout/Layout';
 import { useAuth } from '../utils/customHooks';
 
 const CreateRestaurant = () => {
   useAuth('/restaurants', ['admin']);
+
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.currentUser);
+  const { shifts } = useSelector((state) => state.shifts);
+  const { categories } = useSelector((state) => state.categories);
+
+  useEffect(() => {
+    if (user) {
+      if (shifts.length === 0) {
+        dispatch(fetchShifts(user));
+      }
+      if (categories.length === 0) {
+        dispatch(fetchCategories(user));
+      }
+    }
+    console.log(shifts, categories);
+  }, [shifts, categories]);
 
   return (
     <Layout>
