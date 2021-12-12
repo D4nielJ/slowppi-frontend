48386ff940f3f030c65761d3babb4c5118c3a047
leonardo <leonardodias@ucl.br>
2021-12-12 16:25:28 -0300

    create link to delete restaurants page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
 import { useAuth } from '../utils/customHooks';
 
 const Admin = () => {
   useAuth('/restaurants', ['admin']);
-
   return (
     <Layout>
       <div>Hello world</div>
+      <Link to="/admin/delete">
+        <button type="button">DELETE PAGE</button>
+      </Link>
     </Layout>
   );
 };
