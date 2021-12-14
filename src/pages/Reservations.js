2b0b81469a0f1c9f185f29c8966cc929db83e4d6
Alonso Falconi <alonso.fl@live.com>
2021-12-14 17:10:57 -0500

    Finish Cancel Reservation page

--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   HStack,
@@ -7,6 +7,12 @@ import {
   List,
   ListItem,
   Button,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
 } from '@chakra-ui/react';
 import { useAuth } from '../utils/customHooks';
 import Layout from '../components/Layout/Layout';
@@ -22,18 +28,22 @@ const Reservations = () => {
   const { restaurantslist } = useSelector((state) => state.restaurants);
 
   const [reservationsAvail, setReservationsAvail] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = useRef(null);
+  const [resToDelete, setResToDelete] = useState(null);
+
+  const api = createApi(user.token);
+  const fetchReservations = async () => {
+    try {
+      const { data } = await api.get('v1/reservations/');
+      setReservationsAvail(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const api = createApi(user.token);
-    const fetchReservations = async () => {
-      try {
-        const { data } = await api.get('v1/reservations/');
-        setReservationsAvail(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
     if (user) {
       fetchReservations();
     }
@@ -47,17 +57,30 @@ const Reservations = () => {
     }
   }, [restaurantslist]);
 
-
-  let filteredArray = {};
-  restaurantslist.forEach(({id, name}) => {
-    filteredArray[id] = name;
+  const restaurantsSerialize = {};
+  restaurantslist.forEach(({ id, name }) => {
+    restaurantsSerialize[id] = name;
   });
 
-  console.log(reservationsAvail);
+  const handleCancelReservation = () => {
+    const { token } = user ?? null;
+    const cancelReservation = async () => {
+      const api = createApi(token);
+      await api.delete(`v1/reservations/${resToDelete}`);
+      fetchReservations();
+    };
+    cancelReservation();
+    setIsOpen(false);
+  };
+
+  const openPopup = (id) => {
+    setIsOpen(true);
+    setResToDelete(id);
+  };
 
   return (
     <Layout>
-      <HStack w="full" h="full" justify="space-between">
+      <HStack w="full" h="full" justify="center">
         <Flex h="full" direction="column" pt={20}>
           <Text
             mb={4}
@@ -67,16 +90,43 @@ const Reservations = () => {
           >
             RESERVATIONS
           </Text>
+          <List w="full" rounded={10} backgroundColor="gray.100">
             {reservationsAvail && reservationsAvail.map((res) => (
-              <List w="full" rounded={10} backgroundColor="gray.100">
-                <ListItem key={res.id} id={res.id} p={1} pl={3}>
+
+              <ListItem key={res.id} id={res.id} p={1} pl={3}>
                 <HStack justify="space-between">
-                  <Text as="h3" fontWeight="bold" color="gray.700" fontSize={20}>{filteredArray[res.restaurant_id]}</Text>
-                  <Button colorScheme="red" variant="ghost" type="button">DELETE</Button>
+                  <Text as="h3" fontWeight="bold" color="gray.700" fontSize={20}>{restaurantsSerialize[res.restaurant_id]}</Text>
+                  <Button colorScheme="red" variant="ghost" type="button" onClick={() => openPopup(res.id)}>DELETE</Button>
                 </HStack>
-               </ListItem>
-              </List>
+              </ListItem>
             ))}
+          </List>
+          <AlertDialog
+            isOpen={isOpen}
+            leastDestructiveRef={cancelRef}
+            onClose={onClose}
+          >
+            <AlertDialogOverlay>
+              <AlertDialogContent>
+                <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                  Cancel Reservation
+                </AlertDialogHeader>
+
+                <AlertDialogBody>
+                  Are you sure? You cannot undo this action.
+                </AlertDialogBody>
+
+                <AlertDialogFooter>
+                  <Button ref={cancelRef} onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button colorScheme="red" onClick={handleCancelReservation} ml={3}>
+                    Delete
+                  </Button>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialogOverlay>
+          </AlertDialog>
         </Flex>
       </HStack>
     </Layout>
